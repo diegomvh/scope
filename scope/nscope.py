7e21f758ebd1de6e8fb1bfea4103b06d94f355bf
diegomvh <diegomvh@gmail.com>
2014-06-13 12:54:57 -0300

    Add node object

--- scope/nscope.py
+++ scope/nscope.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python
 
+from .parser import Parser
+
 class Node(object):
-    pass
+    def __init__(self, atoms, parent):
+        self._atoms = atoms
+        self._parent = parent
+        self._retain_count = 1
+
+    def __del__(self):
+        if self._parent is not None:
+            self._parent.release()
+
+    def retain(self):
+	    self._retain_count += 1
+	
+    def release(self):
+        self._retain_count -= 1
+        if self._retain_count == 0:
+	        del self
+
+    def is_auxiliary_scope(self):
+        return self._atoms.startswith("attr.") || self._atoms.startswith("dyn.") 
+    
+    def number_of_atoms(self):
+	    return self._atoms.count(".")
 
 class Scope(object):
     pass
@@ -9,8 +32,10 @@ class Scope(object):
 wildcard = Scope("x-any")
 
 class Selector(object):
-    def _setup(self, source):
-        self._selector = Parser.selector(source)
+    def __ init__(self, source = None):
+        self._selector = None
+        if source is not None:
+            self._selector = Parser.selector(source)
 
     def __str__(self):
         return self._selector and six.text_type(self._selector) or ""
