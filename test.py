8b9e5354e5622c59b1663ebd62e134ccebd1568f
diegomvh <diegomvh@gmail.com>
2014-06-13 13:09:12 -0300

    Add functions to Scope and Nodes

--- test.py
+++ test.py
@@ -70,4 +70,6 @@ class ScopeSelectorTests(unittest.TestCase):
             Scope.factory("source.python punctuation.definition.list.end.python")), rank))
             
 if __name__ == '__main__':
+    from scope.nscope import Scope
+    print(Scope("source.python punctuation.definition.list.begin.python"))
     unittest.main()
